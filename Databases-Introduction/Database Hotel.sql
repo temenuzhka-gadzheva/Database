0c87de41744684b96daf4cf339db319a988d4f53
TemenuzhkaG <temenujka_211@abv.bg>
2021-08-24 15:23:43 +0300

    database introduction

--- Databases-Introduction/Database Hotel.sql
+++ Databases-Introduction/Database Hotel.sql
@@ -3,27 +3,27 @@ CREATE DATABASE Hotel
 USE Hotel
 
 CREATE TABLE Employees(
-Id INT PRIMARY KEY IDENTITY NOT NULL,
-FirstName VARCHAR(50),
-LastName VARCHAR(50),
+Id INT IDENTITY ,
+FirstName VARCHAR(50) NOT NULL,
+LastName VARCHAR(50) NOT NULL,
 Title VARCHAR(50),
 Notes VARCHAR(MAX)
 )
   
 CREATE TABLE Customers(
-Id INT PRIMARY KEY IDENTITY NOT NULL,
-AccountNumber BIGINT,
-FirstName VARCHAR(50),
-LastName VARCHAR(50),
-PhoneNumber VARCHAR(15),
-EmergencyName VARCHAR(150),
-EmergencyNumber VARCHAR(15),
+Id INT IDENTITY,
+AccountNumber BIGINT NOT NULL,
+FirstName VARCHAR(50)NOT NULL,
+LastName VARCHAR(50) NOT NULL,
+PhoneNumber VARCHAR(15) NOT NULL,
+EmergencyName VARCHAR(150) NOT NULL,
+EmergencyNumber VARCHAR(15) NOT NULL,
 Notes VARCHAR(100)
 )
 
  
 CREATE TABLE RoomStatus(
-Id INT PRIMARY KEY IDENTITY NOT NULL,
+Id INT IDENTITY ,
 RoomStatus BIT,
 Notes VARCHAR(MAX)
 )
@@ -39,50 +39,50 @@ Notes VARCHAR(MAX)
 )
   
 CREATE TABLE Rooms (
-RoomNumber INT PRIMARY KEY IDENTITY NOT NULL,
-RoomType VARCHAR(50) FOREIGN KEY REFERENCES RoomTypes(RoomType),
-BedType VARCHAR(50) FOREIGN KEY REFERENCES BedTypes(BedType),
+RoomNumber INT IDENTITY ,
+RoomType VARCHAR(50) NOT NULL,
+BedType VARCHAR(50) NOT NULL,
 Rate DECIMAL(6,2),
 RoomStatus NVARCHAR(50),
 Notes NVARCHAR(MAX)
 )
   
 CREATE TABLE Payments(
-Id INT PRIMARY KEY IDENTITY NOT NULL,
-EmployeeId INT FOREIGN KEY REFERENCES Employees(Id),
-PaymentDate DATE,
-AccountNumber BIGINT,
-FirstDateOccupied DATE,
-LastDateOccupied DATE,
+Id INT IDENTITY ,
+EmployeeId INT NOT NULL,
+PaymentDate DATE NOT NULL,
+AccountNumber BIGINT NOT NULL,
+FirstDateOccupied DATE NOT NULL,
+LastDateOccupied DATE NOT NULL,
 TotalDays AS DATEDIFF(DAY, FirstDateOccupied, LastDateOccupied),
 AmountCharged DECIMAL(14,2),
-TaxRate DECIMAL(8, 2),
-TaxAmount DECIMAL(8, 2),
-PaymentTotal DECIMAL(15, 2),
+TaxRate DECIMAL(8, 2) NOT NULL,
+TaxAmount DECIMAL(8, 2) NOT NULL,
+PaymentTotal DECIMAL(15, 2) NOT NULL,
 Notes VARCHAR(MAX)
 )
  
  
 CREATE TABLE Occupancies(
-Id  INT PRIMARY KEY IDENTITY NOT NULL,
-EmployeeId INT FOREIGN KEY REFERENCES Employees(Id),
+Id INT IDENTITY ,
+EmployeeId INT NOT NULL,
 DateOccupied DATE,
 AccountNumber BIGINT,
-RoomNumber INT FOREIGN KEY REFERENCES Rooms(RoomNumber),
+RoomNumber INT NOT NULL,
 RateApplied DECIMAL(6,2),
 PhoneCharge DECIMAL(6,2),
 Notes VARCHAR(MAX)
 )
  
 
-INSERT INTO Employees
+INSERT INTO Employees(FirstName, LastName, Title, Notes)
 VALUES
 ('Peter', 'Parkar', 'Receptionist', 'Nice customer'),
 ('Natasha', 'Romanov', 'Concierge', 'Nice one'),
 ('Elisaveta', 'Bagriana', 'Cleaner', 'Poetesa')
 
  
-INSERT INTO Customers
+INSERT INTO Customers(AccountNumber, FirstName, LastName, PhoneNumber, EmergencyName, EmergencyNumber, Notes)
 VALUES
 (123456789, 'Jonny', 'Smith', '0887170702', 'Sistry mi', '7708315342', 'Kinky'),
 (123480933, 'Samantha', 'Isaeva', '0893333256', 'Sistry mi', '7708315342', 'Lawer'),
@@ -90,9 +90,9 @@ VALUES
 
 INSERT INTO RoomStatus(RoomStatus, Notes)
 VALUES
-(1,'Refill the minibar'),
-(2,'Check the towels'),
-(3,'Move the bed for couple')
+(123,'Refill the minibar'),
+(234,'Check the towels'),
+(345,'Move the bed for couple')
 
 INSERT INTO RoomTypes (RoomType, Notes)
 VALUES
@@ -100,25 +100,36 @@ VALUES
 ('Wedding suite', 'One king size bed'),
 ('Apartment', 'Up to 3 adults and 2 children')
 
-INSERT INTO BedTypes
+INSERT INTO BedTypes(BedType, Notes)
 VALUES
 ('Double', 'One adult and one child'),
 ('King size', 'Two adults'),
 ('Couch', 'One child')
 
-INSERT INTO Rooms (Rate, Notes)
+INSERT INTO Rooms (RoomType, BedType,Rate, Notes)
 VALUES
-(12,'Free'),
-(15, 'Free'),
-(23, 'Cleaning it')
+('Suite','King size',12,'Free'),
+('Wedding suite','Couch',15, 'Free'),
+('Apartment','Double',23, 'Cleaning it')
 
-INSERT INTO Payments (EmployeeId, PaymentDate, AmountCharged)
+INSERT INTO Payments (EmployeeId, PaymentDate, AccountNumber, FirstDateOccupied, LastDateOccupied, TaxRate, TaxAmount, PaymentTotal)
 VALUES
-(1, '2014-12-15', 2450.10),
-(2, '2016-04-23', 100.50),
-(3, '2021-03-01', 1200.60)
+(2,'2014-12-15',123,'2014-12-12','2014-12-13',2450.10,122.33,456.77),
+(3,'2016-04-23',234,'2016-04-22','2016-04-25',100.50,45.87,765.79),
+(1,'2021-03-01',456,'2021-03-03','2021-03-05',1200.60,567.65,465.92)
 
-INSERT INTO Occupancies (EmployeeId, RateApplied, Notes) VALUES
-(1, 55.55, 'too'),
-(2, 15.55, 'much'),
-(3, 35.55, 'typing')
+INSERT INTO Occupancies (EmployeeId,RoomNumber,RateApplied, Notes) VALUES
+(1,3,55.55, 'too'),
+(2,1,15.55, 'much'),
+(3,2,35.55, 'typing')
+
+/*descrease tax rate*/
+
+UPDATE Payments
+SET TaxRate = TaxRate - (TaxRate * 0.03)
+
+SELECT TaxRate FROM Payments
+
+
+/*delete all records from the Occupancies*/
+ DELETE FROM Occupancies
--- Databases-Introduction/Database Softuni.sql
+++ Databases-Introduction/Database Softuni.sql
@@ -3,26 +3,23 @@ CREATE DATABASE SoftUni
 USE SoftUni
 
 CREATE TABLE Towns (
-	Id INT PRIMARY KEY ,
+	Id INT IDENTITY ,
     [Name] VARCHAR(45)
 )
  
 CREATE TABLE Addresses (
-	Id INT PRIMARY KEY ,
+	Id INT IDENTITY ,
     AddressText VARCHAR(45),
-    TownId INT,
-    CONSTRAINT fk_Addresses_Towns
-    FOREIGN KEY (TownId)
-    REFERENCES Towns(Id)
+    TownId INT
 );
  
 CREATE TABLE Departments (
-	Id INT PRIMARY KEY ,
+	Id INT IDENTITY ,
     [Name] VARCHAR(45)
 );
  
 CREATE TABLE Employees (
-   Id INT PRIMARY KEY ,
+	Id INT IDENTITY ,
    FirstName VARCHAR(30) NOT NULL,
    MiddleName VARCHAR(30) NOT NULL,
    LastName VARCHAR(30) NOT NULL,
@@ -30,35 +27,65 @@ CREATE TABLE Employees (
    DepartmentId INT NOT NULL,
    HireDate DATE,
    Salary DECIMAL(19, 2),
-    AddressId INT,
-    CONSTRAINT fk_Employees_Departments
-    FOREIGN KEY (DepartmentId)
-    REFERENCES Departments(Id),
-    CONSTRAINT fk_Employees_Addresses
-    FOREIGN KEY (AddressId)
-    REFERENCES Addresses(Id)
+    AddressId INT
 );
  
-INSERT INTO Towns(Id,[Name])
+INSERT INTO Towns([Name])
 VALUES
-(1, 'Sofia'),
-(2, 'Plovdiv'),
-(3, 'Varna'),
-(4, 'Burgas');
+('Sofia'),
+('Plovdiv'),
+('Varna'),
+('Burgas');
  
-INSERT INTO Departments(Id,[Name])
+INSERT INTO Departments([Name])
 VALUES
-(1, 'Engineering'), 
-(2, 'Sales'),
-(3, 'Marketing'),
-(4, 'Software Development'),
-(5, 'Quality Assurance');
+('Engineering'), 
+('Sales'),
+('Marketing'),
+('Software Development'),
+('Quality Assurance');
  
-INSERT INTO Employees (Id, FirstName, MiddleName, LastName, JobTitle, DepartmentId, HireDate, Salary, AddressId)
+INSERT INTO Employees (FirstName, MiddleName, LastName, JobTitle, DepartmentId, HireDate, Salary, AddressId)
 VALUES
-(1, 'Ivan', 'Ivanov', 'Ivanov', '.NET Developer', 4, '2013-02-01', 3500.00, 3),
-(2, 'Petar', 'Petrov', 'Petrov', 'Senior Engineer', 1, '2004-03-02', 4000.00,4),
-(3, 'Maria', 'Petrova', 'Ivanova', 'Intern', 5, '2016-08-28', 525.25, 1),
-(4, 'Georgi', 'Terziev', 'Ivanov', 'CEO', 2, '2007-12-09', 3000.00,5),
-(5, 'Peter', 'Pan', 'Pan', 'Intern', 3, '2016-08-28', 599.88, 2);
+('Ivan', 'Ivanov', 'Ivanov', '.NET Developer', 4, '2013-02-01', 3500.00, 3),
+('Petar', 'Petrov', 'Petrov', 'Senior Engineer', 1, '2004-03-02', 4000.00,4),
+('Maria', 'Petrova', 'Ivanova', 'Intern', 5, '2016-08-28', 525.25, 1),
+('Georgi', 'Terziev', 'Ivanov', 'CEO', 2, '2007-12-09', 3000.00,5),
+('Peter', 'Pan', 'Pan', 'Intern', 3, '2016-08-28', 599.88, 2);
 
+/*select all fields*/
+
+SELECT * FROM Towns
+SELECT * FROM Departments
+SELECT * FROM Employees
+
+/*select all fields alphabetically by name for Towns and Deparrtments*/
+
+SELECT * FROM Towns
+ ORDER BY [Name] ASC
+
+ SELECT * FROM Departments
+  ORDER BY [Name] ASC
+
+  /*select all fields descending by salary*/
+
+  SELECT * FROM Employees
+  ORDER BY Salary DESC
+
+  /*selsct some fileds from table*/
+
+  SELECT [Name] FROM Towns
+  ORDER BY [Name] ASC
+
+  SELECT [Name] FROM Departments
+    ORDER BY [Name] ASC
+
+  SELECT FirstName, LastName, JobTitle, Salary FROM Employees
+  ORDER BY Salary DESC
+
+  /*increase employees salary*/
+
+  UPDATE Employees
+  SET Salary *=  1.1 
+
+  SELECT Salary FROM Employees

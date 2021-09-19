4c3f31d73ba3990d99a915cec1bdcc43748f9709
TemenuzhkaG <temenujka_211@abv.bg>
2021-09-19 12:53:53 +0300

    Indices and Data Aggregation

--- Indices and Data Aggregation/Gringotts.sql
+++ Indices and Data Aggregation/Gringotts.sql
@@ -79,4 +79,68 @@ GROUP BY DepositGroup, IsDepositExpired
 ORDER BY DepositGroup DESC, IsDepositExpired
 
 /* Rich Wizard, Poor Wizard */
+SELECT SUM(Guest.DepositAmount - Host.DepositAmount) AS [SumDifference]
+      FROM WizzardDeposits Host
+JOIN WizzardDeposits  Guest ON Host.Id = Guest.Id + 1
+
+USE SoftUni
+/* Departments Total Salaries */
+SELECT e.DepartmentID, SUM(Salary) TotalSalary
+     FROM Employees e
+GROUP BY e.DepartmentID
+
+/* Employees Minimum Salaries */
+SELECT DepartmentID, MIN(Salary) AS MinimumSalary
+     FROM Employees 
+WHERE DepartmentID IN (2,5,7) AND HireDate > '01/01/2000'
+GROUP BY DepartmentID
+
+/* Employees Average Salaries */
+
+SELECT * INTO SalaryOver30000 
+      FROM Employees e
+WHERE Salary > 30000
+
+DELETE FROM SalaryOver30000
+WHERE ManagerID = 42
+
+UPDATE SalaryOver30000 
+SET Salary += 5000
+WHERE DepartmentID = 1
+
+SELECT DepartmentID, AVG(Salary)
+     FROM SalaryOver30000
+GROUP BY DepartmentID
+
+
+/* Employees Maximum Salaries */
+SELECT  DepartmentId, MAX(Salary) AS MaxSalary
+      FROM Employees 
+GROUP BY DepartmentId
+HAVING MAX(Salary) NOT BETWEEN 30000 AND 70000
+
+/* Employees Count Salaries */
+SELECT COUNT(*) AS [Count]
+      FROM Employees 
+WHERE ManagerID IS NULL
+
+/* 3rd Highest Salary */
+SELECT DepartmentId, Salary AS [ThirdHighestSalary] 
+    FROM  (SELECT DepartmentID, 
+                  Salary, 
+	              DENSE_RANK() OVER(PARTITION BY DepartmentId ORDER BY Salary DESC) AS Ranked 
+                  FROM Employees) AS ThirdSalary
+WHERE ThirdSalary.Ranked = 3
+GROUP BY DepartmentID, Salary
+
+
+/* Salary Challenge */
+SELECT Top(10) FirstName, LastName,DepartmentID
+     FROM Employees e
+	 WHERE e.Salary > 
+	     (SELECT AVG(Salary)   
+		        FROM Employees
+		 WHERE DepartmentID = e.DepartmentID 
+		 GROUP BY DepartmentID)
+
 

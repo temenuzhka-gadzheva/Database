a42424e4de4442573825b5200129ffe98b8cdd17
Temenuzhka <temenujka_211@abv.bg>
2022-01-24 21:41:09 +0200

    fix some problems of  AutoMappingObjectsProject

---
+++ Entity Framework Core/C# Auto Mapping Objects/AutoMappingObjectsProject- ExDemo/FastFood.Core/Controllers/EmployeesController.cs
@@ -0,0 +1,61 @@
+﻿namespace FastFood.Core.Controllers
+{
+    using System;
+    using System.Linq;
+    using AutoMapper;
+    using AutoMapper.QueryableExtensions;
+    using Data;
+    using FastFood.Models;
+    using Microsoft.AspNetCore.Mvc;
+    using ViewModels.Employees;
+
+    public class EmployeesController : Controller
+    {
+        private readonly FastFoodContext context;
+        private readonly IMapper mapper;
+
+        public EmployeesController(FastFoodContext context, IMapper mapper)
+        {
+            this.context = context;
+            this.mapper = mapper;
+        }
+
+        public IActionResult Register()
+        {
+            var positions = this.context.Positions
+                .ProjectTo<RegisterEmployeeViewModel>(mapper.ConfigurationProvider)
+                .ToList();
+
+            return this.View(positions);
+        }
+
+        [HttpPost]
+        public IActionResult Register(RegisterEmployeeInputModel model)
+        {
+            // with hand
+            /* var employee = new Employee
+             {
+                 Name = model.Name,
+                 Address = model.Address,
+                 Age = model.Age,
+                 PositionId = model.PositionId
+             };*/
+
+            // with mapper
+            var employee = this.mapper.Map<Employee>(model);
+            this.context.Employees.Add(employee);
+            this.context.SaveChanges();
+
+            return this.RedirectToAction("All", "Employees");
+        }
+
+        public IActionResult All()
+        {
+            var employees = this.context.Employees
+                .ProjectTo<EmployeesAllViewModel>(this.mapper.ConfigurationProvider)
+                .ToList();
+
+            return this.View(employees);
+        }
+    }
+}

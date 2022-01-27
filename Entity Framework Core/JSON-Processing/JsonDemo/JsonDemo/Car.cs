25965b1111d45283c9446df73bb72ce77bd32a2e
Temenuzhka <temenujka_211@abv.bg>
2022-01-27 22:11:02 +0200

    json processing

--- Entity Framework Core/JSON-Processing/JsonDemo/JsonDemo/Car.cs
+++ Entity Framework Core/JSON-Processing/JsonDemo/JsonDemo/Car.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,10 +13,17 @@ namespace JsonDemo
         {
             this.Extras = new List<string>();
         }
+        // order of properties
+        // changing of property of name
+        // how to cope with null values
+        [JsonProperty(Order = 10, PropertyName ="test", NullValueHandling = NullValueHandling.Ignore)]
         public string Vendor { get; set; }
         public string Model { get; set; }
         public decimal Price { get; set; }
         public DateTime ManufacturedOn { get; set; }
+        // ignore in json format
+        // use Newtonsoft Json using
+        [JsonIgnore]
         public List<string> Extras { get; set; }
         public Engine Engine { get; set; }
     }

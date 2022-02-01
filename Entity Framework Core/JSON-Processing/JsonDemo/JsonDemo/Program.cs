cb1ddf7b4c611ecb12f4a1fb3a7b7ec7952a11f9
Temenuzhka <temenujka_211@abv.bg>
2022-02-01 16:18:34 +0200

    jsonDemo

--- Entity Framework Core/JSON-Processing/JsonDemo/JsonDemo/Program.cs
+++ Entity Framework Core/JSON-Processing/JsonDemo/JsonDemo/Program.cs
@@ -1,11 +1,14 @@
-﻿using Newtonsoft.Json;
+﻿using CsvHelper;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using Newtonsoft.Json.Serialization;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO;
 using System.Linq;
 using System.Text.Json;
+using System.Xml;
 /*using System.Text.Json;
 using System.Text.Json.Serialization;*/
 
@@ -25,50 +28,8 @@ namespace JsonDemo
                 Engine = new Engine { HorsePower = 333, Volume = 2.3F }
             };*/
 
-
             // Console.WriteLine(JsonConvert.SerializeObject(car, Formatting.Indented));
-
-            // json work with anonymous types
-            // Console.WriteLine(JsonConvert.SerializeObject(new { Car = car, Name = "Marty"}, Formatting.Indented));
-
-            // settings of json
-            //  naming of properties
-            //  date formatting
-            //  titming zones
-            /*  var settings = new JsonSerializerSettings
-              {
-                  Formatting = Formatting.Indented,
-                  ContractResolver = new DefaultContractResolver
-                  {
-                      NamingStrategy = new CamelCaseNamingStrategy()
-                  },
-                  DateFormatString = "yyyy-MM-dd",
-                  DateTimeZoneHandling = DateTimeZoneHandling.Utc
-
-              };
-              Console.WriteLine(JsonConvert.SerializeObject(car,settings));
-
-              */
-
-
-            //  linq to json  & json to linq
-            /*var json = File.ReadAllText("myCar.json");
-            var jObject =  JObject.Parse(json);
-            // jObject.Children().Where(x => x)
-
-            foreach (var item in jObject.Children().Where(x => x.Children().Count() > 0))
-            {
-                Console.WriteLine(item);
-                Console.WriteLine(new string('-', 60));
-            }
-            var car = JsonConvert.DeserializeObject<Car>(json);*/
-
-
-            // json to xml & xml to json
-            // can root object
-            var json = File.ReadAllText("myCar.json");
-             var xmlDoc = JsonConvert.DeserializeXmlNode(json);
-            // xml to 
+            //--------------------------------------------------------------------------------
 
             // using syste.Text.Json
             // File.WriteAllText("myCar.json",JsonSerializer.Serialize(car));
@@ -87,99 +48,92 @@ namespace JsonDemo
 
              var carFromJson = JsonConvert.DeserializeObject<Car>(json);*/
 
+            //------------------------------------------------------------------------------------
+
+            // json work with anonymous types
+            // Console.WriteLine(JsonConvert.SerializeObject(new { Car = car, Name = "Marty"}, Formatting.Indented));
+            //---------------------------------------------------------------------------------------------------------
+
+            // settings of json
+            //  naming of properties
+            //  date formatting
+            //  titming zones
+            /*  var settings = new JsonSerializerSettings
+              {
+                  Formatting = Formatting.Indented,
+                  ContractResolver = new DefaultContractResolver
+                  {
+                      NamingStrategy = new CamelCaseNamingStrategy()
+                  },
+                  DateFormatString = "yyyy-MM-dd",
+                  DateTimeZoneHandling = DateTimeZoneHandling.Utc
+
+              };*/
+            //  Console.WriteLine(JsonConvert.SerializeObject(car,settings));
+            //------------------------------------------------------------------
+
+
+
+            //  linq to json  & json to linq
+            /*var json = File.ReadAllText("myCar.json");
+            var jObject =  JObject.Parse(json);
+            // jObject.Children().Where(x => x)
+
+            foreach (var item in jObject.Children().Where(x => x.Children().Count() > 0))
+            {
+                Console.WriteLine(item);
+                Console.WriteLine(new string('-', 60));
+            }
+            var car = JsonConvert.DeserializeObject<Car>(json);*/
+            //-----------------------------------------------------------------------------------
+
+            // json to xml & xml to json
+            // can root object
+            // var json = File.ReadAllText("myCar.json");
+            //  var xmlDoc = JsonConvert.DeserializeXmlNode(json);
+
+            // xml to json
+            /* var xml = new XmlDocument();
+             var root = xml.AppendChild(xml.CreateElement("test", "testov"));
+             xml.AppendChild(root);
+             root.AppendChild(xml.CreateElement("pesho", "peshev"));
+             root.AppendChild(xml.CreateElement("admin", "admin"));
+
+             Console.WriteLine(JsonConvert.SerializeXmlNode(xml, Newtonsoft.Json.Formatting.Indented));*/
+
+            //------------------------------------------------------------------------------------------------------
+
+            // with edit  paste special we can convert json (with double "" "") as classes
+            // for example
             /* var obj = JsonConvert.DeserializeObject<Rootobject>(@"{""web-app"": {
-     ""servlet"": [
-       {
-                 ""servlet - name"": ""cofaxCDS"",
-         ""servlet -class"": ""org.cofax.cds.CDSServlet"",
-         ""init-param"": {
-           ""configGlossary:installationAt"": ""Philadelphia, PA"",
-           ""configGlossary:adminEmail"": ""ksm @pobox.com"",
-           ""configGlossary:poweredBy"": ""Cofax"",
-           ""configGlossary:poweredByIcon"": ""/images/cofax.gif"",
-           ""configGlossary:staticPath"": ""/content/static"",
-           ""templateProcessorClass"": ""org.cofax.WysiwygTemplate"",
-           ""templateLoaderClass"": ""org.cofax.FilesTemplateLoader"",
-           ""templatePath"": ""templates"",
-           ""templateOverridePath"": """",
-           ""defaultListTemplate"": ""listTemplate.htm"",
-           ""defaultFileTemplate"": ""articleTemplate.htm"",
-           ""useJSP"": false,
-           ""jspListTemplate"": ""listTemplate.jsp"",
-           ""jspFileTemplate"": ""articleTemplate.jsp"",
-           """"cachePackageTagsTrack"""": 200,
-           ""cachePackageTagsStore"": 200,
-           ""cachePackageTagsRefresh"": 60,
-           ""cacheTemplatesTrack"": 100,
-           ""cacheTemplatesStore"": 50,
-           ""cacheTemplatesRefresh"": 15,
-           ""cachePagesTrack"": 200,
-           ""cachePagesStore"": 100,
-           ""cachePagesRefresh"": 10,
-           ""cachePagesDirtyRead"": 10,
-           ""searchEngineListTemplate"": ""forSearchEnginesList.htm"",
-           ""searchEngineFileTemplate"": ""forSearchEngines.htm"",
-           ""searchEngineRobotsDb"": ""WEB-INF/robots.db"",
-           ""useDataStore"": true,
-           ""dataStoreClass"": ""org.cofax.SqlDataStore"",
-           ""redirectionClass"": ""org.cofax.SqlRedirection"",
-           ""dataStoreName"": ""cofax"",
-           ""dataStoreDriver"": ""com.microsoft.jdbc.sqlserver.SQLServerDriver"",
-           ""dataStoreUrl"": ""jdbc:microsoft:sqlserver://LOCALHOST:1433;DatabaseName=goon"",
-           ""dataStoreUser"": ""sa"",
-           ""dataStorePassword"": ""dataStoreTestQuery"",
-           ""dataStoreTestQuery"": ""SET NOCOUNT ON;select test = 'test';"",
-           ""dataStoreLogFile"": ""/usr/local/tomcat/logs/datastore.log"",
-           ""dataStoreInitConns"": 10,
-           ""dataStoreMaxConns"": 100,
-           ""dataStoreConnUsageLimit"": 100,
-           ""dataStoreLogLevel"": ""debug"",
-           ""maxUrlLength"": 500}
- },
-       {
-     ""servlet - name"": ""cofaxEmail"",
-         ""servlet -class"": ""org.cofax.cds.EmailServlet"",
-         ""init - param"": {
-     ""mailHost"": ""mail1"",
-         ""mailHostOverride"": ""mail2""}},
-       {
-     ""servlet - name"": ""cofaxAdmin"",
-         ""servlet -class"": ""org.cofax.cds.AdminServlet""},
-
-       {
-     ""servlet - name"": ""fileServlet"",
-         ""servlet -class"": ""org.cofax.cds.FileServlet""},
-       {
-     ""servlet - name"": ""cofaxTools"",
-         ""servlet -class"": ""org.cofax.cms.CofaxToolsServlet"",
-         ""init - param"": {
-     ""templatePath"": ""toolstemplates / "",
-           ""log"": 1,
-           ""logLocation"": "" / usr / local / tomcat / logs / CofaxTools.log"",
-           ""logMaxSize"": """",
-           ""dataLog"": 1,
-           ""dataLogLocation"": "" / usr / local / tomcat / logs / dataLog.log"",
-           ""dataLogMaxSize"": """",
-           ""removePageCache"": "" / content / admin / remove ? cache = pages & id = "",
-           ""removeTemplateCache"": "" / content / admin / remove ? cache = templates & id = "",
-           ""fileTransferFolder"": "" / usr / local / tomcat / webapps / content / fileTransferFolder"",
-           ""lookInContext"": 1,
-           ""adminGroupID"": 4,
-           ""betaServer"": true}}],
-     ""servlet - mapping"": {
-     ""cofaxCDS"": "" / "",
-       ""cofaxEmail"": "" / cofaxutil / aemail/*"",
-       ""cofaxAdmin"": ""/admin/*"",
-       ""fileServlet"": ""/static/*"",
-       ""cofaxTools"": ""/tools/*""},
-
-     ""taglib"": {
-       ""taglib-uri"": ""cofax.tld"",
-       ""taglib-location"": ""/WEB-INF/tlds/cofax.tld""}}}");
-
-            */
+               ""servlet"": [
+                   {
+                      ""servlet - name"": ""cofaxCDS"",
+                       ""servlet -class"": ""org.cofax.cds.CDSServlet""
+                   }
+                           ]
+              }");*/
 
 
+            //------------------------------------------------------------------------------------------------------
+
+            // format csv
+            // install csv helper
+            // generate class
+            using (var reader = new StreamReader("Departments.csv"))
+            using (var csv = new CsvReader(reader, CultureInfo.InvariantCulture))
+            {
+                var record = csv.GetRecords<Department>();
+            }
+
+        }
+        class Department
+        {
+            public int DepartmentId { get; set; }
+            public string Name { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+            public decimal Salary { get; set; }
         }
     }
 

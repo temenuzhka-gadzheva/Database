4b7297e50f32a203e866425fa87a9b65000c56a7
TemenuzhkaG <temenujka_211@abv.bg>
2022-03-14 13:21:45 +0200

    some changes

--- Entity Framework Core/C# Auto Mapping Objects/AutoMappingObjectsProject- ExDemo/FastFood.Data/FastFoodContextDesignTimeFactory.cs
+++ Entity Framework Core/C# Auto Mapping Objects/AutoMappingObjectsProject- ExDemo/FastFood.Data/FastFoodContextDesignTimeFactory.cs
@@ -1,8 +1,9 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System.IO;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Design;
 using Microsoft.Extensions.Configuration;
-using System;
-using System.IO;
+
+
 
 namespace FastFood.Data
 {
@@ -11,19 +12,22 @@ namespace FastFood.Data
         public FastFoodContext CreateDbContext(string[] args)
         { // second variant 
 
-          /*  var configuration = new ConfigurationBuilder()
-                .SetBasePath(Directory.GetCurrentDirectory())
-                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-                .Build();
+          /*  var configuration = new  ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory())
+                    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                    .Build();
 
             var builder = new DbContextOptionsBuilder<FastFoodContext>();
             var connectionString = configuration.GetConnectionString("DefaultConnection");
-          */
+            builder.UseSqlServer(connectionString);
+
+            return new FastFoodContext(builder.Options);*/
+
 
             // first variant
              var builder = new DbContextOptionsBuilder<FastFoodContext>();
-             builder.UseSqlServer("Server=.;Database=FastFood;Trusted_Connection=True;MultipleActiveResultSets=true");
-
+            builder.UseSqlServer("Server=.;Database=FastFood;Trusted_Connection=True;MultipleActiveResultSets=true");
+           // builder.UseSqlServer(connectionString);
 
             return new FastFoodContext(builder.Options);
         }
